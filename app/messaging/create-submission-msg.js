23a705795f000441636d934c7dd36451ced23936
yunusfirat <yunusfirat44@gmail.com>
2022-08-23 16:34:03 +0100

    added space to m2

--- app/messaging/create-submission-msg.js
+++ app/messaging/create-submission-msg.js
@@ -217,7 +217,7 @@ function getEmailDetails(submission, desirabilityScore, rpaEmail, isAgentEmail =
       existingStorageCapacity:submission.existingStorageCapacity,
       plannedStorageCapacity:submission.plannedStorageCapacity,
       cover: submission.cover ?? ' ',
-      coverSize: submission.coverSize? submission.coverSize.concat('m²') : 'N/A',
+      coverSize: submission.coverSize? submission.coverSize.concat(' m²') : 'N/A',
       itemSizeQuantities: submission.itemSizeQuantities ? displayObject(submission.itemSizeQuantities, [submission.otherItems].flat()).join('\n') : 'None selected',
       coverType: submission.coverType || 'Not needed',
       storageType: submission.storageType,
