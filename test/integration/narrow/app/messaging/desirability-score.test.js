56676948511b5b6dbb9a5b62ff3da2c5d35e22d5
Julian Taylor <julian.taylor@capgemini.com>
2022-08-23 14:48:36 +0100

    Test desirability score

---
+++ test/integration/narrow/app/messaging/desirability-score.test.js
@@ -0,0 +1,57 @@
+describe('Desirability score', () => {
+  beforeAll(() => {
+    console.log = jest.fn()
+  })
+
+  it('Successful path', async () => {
+    const desirabilityScore = require('../../../../../app/messaging/desirability-score')
+
+    const cacheMock = jest.mock('../../../../../app/cache')
+    cacheMock.setDesirabilityScore = jest.fn(() => { })
+    const msg = {
+      body: {
+        correlationId: 7357
+      },
+      desirabilityScoreMsg: 'lorem ipsum'
+    }
+    const desirabilityScoreReceiver = {
+      completeMessage: jest.fn(msg => { })
+    }
+
+    desirabilityScore(msg, desirabilityScoreReceiver)
+
+    expect(console.log).toHaveBeenCalledWith(msg.body, 'desirabilityScoreMsg')
+    expect(cacheMock.setDesirabilityScore).toHaveBeenCalled()
+    expect(desirabilityScoreReceiver.completeMessage).toHaveBeenCalled()
+  })
+
+  it('Error path', async () => {
+    const desirabilityScore = require('../../../../../app/messaging/desirability-score')
+
+    const cacheMock = jest.mock('../../../../../app/cache')
+    cacheMock.setDesirabilityScore = jest.fn(() => { throw Error })
+
+    const appInsightsMock = require('../../../../../app/services/app-insights')
+    appInsightsMock.logException = jest.fn()
+
+    const msg = {
+      body: {
+        correlationId: 7357
+      },
+      desirabilityScoreMsg: 'lorem ipsum'
+    }
+    const desirabilityScoreReceiver = {
+      abandonMessage: jest.fn()
+    }
+
+    desirabilityScore(msg, desirabilityScoreReceiver)
+
+    await desirabilityScoreReceiver.abandonMessage(msg)
+
+    expect(console.log.mock.calls[0]).toEqual([{ correlationId: 7357 }, 'desirabilityScoreMsg'])
+    expect(console.log.mock.calls[1]).toEqual('[TypeError: Cannot read property \'set\' of undefined]')
+    expect(console.log.mock.calls[2]).toEqual([{ correlationId: 7357 }, 'desirabilityScoreMsg'])
+    expect(appInsightsMock.logException).toHaveBeenCalled()
+    expect(desirabilityScoreReceiver.abandonMessage).toHaveBeenCalled()
+  })
+})
